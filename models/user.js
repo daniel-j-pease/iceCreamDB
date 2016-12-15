67051b561e57ed8f447679f913c58cb9bef360db
Daniel Pease <daniel.j.pease@gmail.com>
2016-12-14 23:49:41 -0500

    move client changes to user model

--- models/user.js
+++ models/user.js
@@ -1,15 +1,17 @@
-const db = require('../lib/db');
+const client = require('../lib/db.js')
 const bcrypt = require('bcryptjs');
 
 const salt = 10;
 
 function createUser(req, res, next) {
   if (req.body.signupPassword === req.body.signupConfirm) {
-    db.none('INSERT INTO users (username, password, total_workouts) VALUES ($1, $2, $3);',
+    client.connect()
+    client.query('INSERT INTO users (username, password, total_workouts) VALUES ($1, $2, $3);',
       [req.body.signupUsername, bcrypt.hashSync(req.body.signupPassword, salt), 0])
       .then( () => {
         res.signupResult = {signup: true}
         next()
+        client.end()
       })
     .catch(error => console.log(error))
   } else {
@@ -21,15 +23,18 @@ function createUser(req, res, next) {
 
 function authenticate(req, res, next) {
   console.log(req.body)
-  db.one('SELECT * FROM users WHERE username = $/loginUsername/;', req.body)
+  client.connect()
+  client.query('SELECT * FROM users WHERE username = $/loginUsername/;', req.body)
     .then((data) => {
       const match = bcrypt.compareSync(req.body.loginPassword, data.password);
       if (match) {
         res.loginResult = data
         next();
+        client.end();
       } else {
         res.loginResult = {failed: "failed"}
         next();
+        client.end();
         return
       }
     })
@@ -38,9 +43,11 @@ function authenticate(req, res, next) {
 
 function incrementTotalWorkouts(req, res, next) {
   console.log(req.body)
-  db.none('UPDATE users SET total_workouts = $/total_workouts/ WHERE username = $/username/;', req.body)
+  client.connect()
+  client.query('UPDATE users SET total_workouts = $/total_workouts/ WHERE username = $/username/;', req.body)
     .then( () => {
       next();
+      client.end();
     })
   .catch(error => console.log(error))
 }
